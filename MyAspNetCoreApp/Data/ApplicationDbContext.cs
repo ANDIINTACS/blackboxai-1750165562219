97da6e032c1b3302b8325c8e94fa4d717f6d15da
ANDIINTACS <95155886+ANDIINTACS@users.noreply.github.com>
2025-06-17 20:06:28 +0700

    Add MyAspNetCoreApp/Data/ApplicationDbContext.cs 1750165588904

---
+++ MyAspNetCoreApp/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+using MyAspNetCoreApp.Models;
+
+namespace MyAspNetCoreApp.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
